e62bfe8e5cc6c105ea9194cc9f99cc80402a2628
Alden Laslett <ablaslett@gmail.com>
2022-07-18 23:18:45 +0100

    refactor schedule notes currentTicks to allow for play, pause, and jump to

--- src/app/noteScheduler.ts
+++ src/app/noteScheduler.ts
@@ -1,9 +1,8 @@
 import { BehaviorSubject, combineLatest, from, Observable, of, timer } from 'rxjs';
-import { MidiEvent, NoteEvent, isOfType } from './events';
-import { distinctUntilChanged, map, scan, share, shareReplay, startWith, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { MidiEvent, NoteEvent } from './events';
+import { distinctUntilChanged, map, share, shareReplay, switchMap, switchScan, withLatestFrom } from 'rxjs/operators';
 import IntervalTree from 'node-interval-tree';
 import { getTimeNow } from './audio-context';
-import { log } from 'rxfm';
 
 const tempoSubject = new BehaviorSubject(120); // bpm
 export const tempo = tempoSubject.pipe(distinctUntilChanged());
@@ -26,35 +25,35 @@ const noteScheduleMargin = 1.2;
 const ticksOffset = new BehaviorSubject(0);
 const playing = new BehaviorSubject(true);
 
-// TODO: Make this an operator which emits the current ticks range?
-let previousTicks = 0;
-const ticksRange = combineLatest([
-  playing,
-  getTimeNow,
-  tempo,
-  ticksOffset.pipe(tap(offset => previousTicks = offset)),
-]).pipe(
-  switchMap(([playing, getTimeNow, tempo, ticksOffset]) => {
-    if (!playing) return of(previousTicks);
-    return timer(0, noteScehdulePeriod).pipe(
-      map(() => {
-        const now = getTimeNow();
-        const ticksNow = secondsToTicks(tempo, now) + ticksOffset;
-        previousTicks = ticksNow;
-        return ticksNow;
-      }),
-    );
-  }),
-);
+// Change ticks offset -> go there
+// Start -> count from ticks offset
+// Pause -> hold at value
+// Play -> count from last value
+// Stop -> return to zero and reset ticks offset
 
-const currentTicks = timer(0, noteScehdulePeriod).pipe(
-  withLatestFrom(getTimeNow),
-  map(([, now]) => now()),
-  // log("timer"),
-  // share(),
+const currentTicks = combineLatest([ticksOffset, getTimeNow]).pipe(
+  switchMap(([ticksOffset, getTimeNow]) => playing.pipe(
+    switchScan((previousTicks, playing) => {
+      if (!playing) return of(previousTicks);
+      const startTime = getTimeNow();
+      return combineLatest([tempo, timer(0, noteScehdulePeriod)]).pipe(
+        // TODO: Changing tempo will scale the output, this should behave the same as ticksOffset and reset from the current position?
+        map(([tempo]) => secondsToTicks(tempo, getTimeNow() - startTime) + ticksOffset),
+      );
+    }, ticksOffset),
+  )),
   shareReplay({ refCount: true, bufferSize: 1 }),
 );
 
+// const currentTicks = timer(0, noteScehdulePeriod).pipe(
+//   withLatestFrom(getTimeNow),
+//   map(([, now]) => now()),
+//   // log("timer"),
+//   // share(),
+//   shareReplay({ refCount: true, bufferSize: 1 }),
+// );
+
+// TODO: Store these as combined start and end events as an interval? Then easier to display.
 const midiEventsToIntervalTree = () => (events: Observable<MidiEvent[]>)  => events.pipe(
   map((events) => {
     const intervalTree = new IntervalTree<NoteEvent>();
@@ -65,24 +64,6 @@ const midiEventsToIntervalTree = () => (events: Observable<MidiEvent[]>)  => eve
     return intervalTree;
   }),
 );
-
-// const midiEventsToIntervalTree = () => (events: Observable<MidiEvent[]>)  => events.pipe(
-//   scan((intervalTree, event) => {
-//     const ticks = event.ticks ?? 0;
-//     if (isOfType(event, "deleteNote", "updateNote")) {
-//       const existingEvent = intervalTree.search(ticks, ticks).filter(({ midiNote }) => midiNote === event.midiNote)[0];
-//       if (!existingEvent) return intervalTree;
-//       intervalTree.remove(ticks, ticks, existingEvent);
-//       if (event.type === "updateNote")
-//         intervalTree.insert(event.newTicks, event.newTicks, { ...existingEvent, ticks: event.newTicks });
-//     } else {
-//       intervalTree.insert(ticks, ticks, event);
-//       console.log(`${event.type} event inserted at: ${event.ticks}`);
-//     }
-//     return intervalTree;
-//   }, new IntervalTree<NoteEvent>()),
-//   distinctUntilChanged(),
-// );
 
 interface ScheduleNotesProps {
   startTicks?: Observable<number>;
@@ -98,13 +79,13 @@ export const scheduleNotes = ({
 }: ScheduleNotesProps = {}) => (events: Observable<MidiEvent[]>) => events.pipe(
   midiEventsToIntervalTree(),
   switchMap(intervalTree => {
-    let ticksEndRange = 0;
+    let ticksRangeEnd = 0;
     return currentTicks.pipe(
       withLatestFrom(tempo),
-      switchMap(([start, tempo]) => {
-        const ticksStartRange = Math.max(secondsToTicks(tempo, start), ticksEndRange);
-        ticksEndRange = secondsToTicks(tempo, start + (noteScehdulePeriod / 1000) * noteScheduleMargin) + 1;
-        const events = intervalTree.search(ticksStartRange, ticksEndRange);
+      switchMap(([ticksStart, tempo]) => {
+        const ticksRangeStart = Math.max(ticksStart, ticksRangeEnd);
+        ticksRangeEnd = ticksStart + secondsToTicks(tempo, (noteScehdulePeriod / 1000) * noteScheduleMargin) + 1;
+        const events = intervalTree.search(ticksRangeStart, ticksRangeEnd);
         // console.log(`${events.length} events found between ${ticksStartRange} and ${ticksEndRange} | start: ${start} end: ${start + (noteScehdulePeriod / 1000) * noteScheduleMargin}`);
         return from(events);
       }),
